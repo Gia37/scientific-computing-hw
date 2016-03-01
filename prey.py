a02f68e379a93fb9d332d5379c31a374b9b057c2
giselaarrietarivera@gmail.com <giselaarrietarivera@gmail.com>
2016-03-01 11:38:56 -0500

    modelo Lokta-Volterra

--- prey.py
+++ prey.py
@@ -15,9 +15,22 @@ Kyx = 0.01
 Kx = 1.06
 Kxy = 0.01
 t = range (101) 
-y = range (101)
+l = range (101)
 
 for i in range (101):
-	y0 = D * Ky * Y0 - D * Kxy * x0 * y0 + y0
-	x0 = D * Kyx * y0 * x0 - D * Kx * k0 + x0
+	y1 = D * Ky * Y0 - D * Kxy * x0 * y0 + y0
+	x1 = D * Kyx * y0 * x0 - D * Kx * k0 + x0
+	y0 = y1
+	x0 = x1
+	t [i] = y1
+	y [i] =  (i * 0.1)
+
+tnew = numpy.array (x)
+lnew = numpy.array (y)
+
+print xnew
+print ynew
+
+matplotlib.pyplot.plot(tnew, lnew)
+matplotlib.pyplot.show ()
 
