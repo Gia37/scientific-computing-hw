74b6a71af318c3c797f9b2500930f49a24e6a8be
giselaarrietarivera@gmail.com <giselaarrietarivera@gmail.com>
2016-03-03 10:19:32 -0500

    depredador y presa

--- prey.py
+++ prey.py
@@ -1,35 +1,45 @@
-#Autor: Gisela Arrieta Rivera
-#Fecha: Mar-01-2016
-#Objetivo:
-#
-
 import numpy
 import matplotlib
 import matplotlib.pyplot
 
-D = 0.1
-x0 = 15
-y0 = 100
-Ky = 2.0
-Kyx = 0.01
-Kx = 1.06
-Kxy = 0.01
-t = range (101) 
-l = range (101)
 
-for i in range (101):
-	y1 = D * Ky * Y0 - D * Kxy * x0 * y0 + y0
-	x1 = D * Kyx * y0 * x0 - D * Kx * k0 + x0
-	y0 = y1
-	x0 = x1
-	t [i] = y1
+ky=2.0
+kyx=0.01
+kx=1.06
+kxy=0.01
+d=0.1
+x0=15
+y0=100
+x=range(101)
+y=range(101)
+t=range(101)
 
-tnew = numpy.array (t)
-lnew = numpy.array (l)
+for i in range(101):
+	y1= d*ky*y0-d*kxy*x0*y0+y0
+	y0=y1
+	x1= d*kyx*y0*x0-d*kx*x0+x0  
+	x0=x1
+        x[i]=x0
+	y[i]=y1
+	t[i]=i*0.1
+	
 
-print xnew
+xnew=numpy.array(x)
+ynew=numpy.array(y)
+tnew=numpy.array(t)
+
+
 print ynew
+print xnew
+print tnew
 
-matplotlib.pyplot.plot(tnew, lnew)
-matplotlib.pyplot.show ()
+
+matplotlib.pyplot.plot(xnew,tnew)
+matplotlib.pyplot.show()
+matplotlib.pyplot.plot(ynew,tnew)
+matplotlib.pyplot.show()
+matplotlib.pyplot.plot(xnew,ynew)
+matplotlib.pyplot.show()
+
+   
 
