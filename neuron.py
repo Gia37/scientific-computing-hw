cfa7684631ca20238e5b25add737f73abc244a77
giselaarrietarivera@gmail.com <giselaarrietarivera@gmail.com>
2016-03-29 11:42:22 -0500

    nuevo problema

--- neuron.py
+++ neuron.py
@@ -20,15 +20,18 @@ T = 6.3
 V = -65
 delta = 0.001
 phi = 3**(T-6.3)/10
-delta_n = phi(0.01(V + 10)/(np.exp((V + 10)/10) -1)
-delta_m = phi(0.01(V + 25)/(np.exp((V + 25)/10) -1)
-delta_h = phi(0.07 np.exp(V/20))
+alpha_n = phi(0.01(V + 10)/(np.exp((V + 10)/10) -1)
+alpha_m = phi(0.01(V + 25)/(np.exp((V + 25)/10) -1)
+alpha_h = phi(0.07 np.exp(V/20))
 n = 0.317
 m = 0.05
 h = 0.6
 beta_n = phi(0.125 np.exp(V/80))
 beta_m = phi(4 np.exp(V/18))
 beta_h = phi(1/(np.exp((V + 30)/10) +1)
+
+
+
 
 
 
