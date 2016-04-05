624270495d6609fd04ca9d4b949ca811afde5ccc
giselaarrietarivera@gmail.com <giselaarrietarivera@gmail.com>
2016-04-05 11:35:25 -0500

    Hallar pi

---
+++ pi.py
@@ -0,0 +1,30 @@
+# Gisela Arrieta Rivera
+# 
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import sys
+
+np.random.seed(0)
+for j in range(7):	
+	print np.random.random()
+
+
+
+R = 1
+L = 3
+N = 100
+N1 = 1000
+N2 = 10000
+pi = (L/R)**2 * (n/N)
+
+
+for n in range(-1.5,1.5):
+	if x: np.sqrt(X**2) + (Y**2)<=R
+	print x + 1
+
+	else y:np.sqrt(X**2) + (Y**2)<=R
+	print y + 1
+
+	
