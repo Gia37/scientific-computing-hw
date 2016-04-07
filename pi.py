d52216efae2261724f6eeb6049a2d55bc4a8cd88
giselaarrietarivera@gmail.com <giselaarrietarivera@gmail.com>
2016-04-07 11:40:44 -0500

    numero pi

--- pi.py
+++ pi.py
@@ -7,24 +7,36 @@ import matplotlib.pyplot as plt
 import sys
 
 np.random.seed(0)
+
 for j in range(7):	
 	print np.random.random()
 # Encontrar un numero aleatorio entre -1.5 y 1.5
 
 
+
 R = 1
 L = 3
 N = 100
-N1 = 1000
-N2 = 10000
-pi = (L/R)**2 * (n/N)
+y = 1.5-np.random.random() * 3
 
 
-for n in range(-1.5,1.5):
-	if x: np.sqrt(X**2) + (Y**2)<=R
-	print x + 1
+def pi(L, R, N):
+	x = np.linspace(0,1, num=N, endpoint='true')
+	n = 0
+x=range(N)
+y=range(N)
+n = 0
+for j in range(N):
+	x[j] = L/2 -np.random.random() * 3
+	y[j] = np.random.uniform(-1.5,1.5)
+	
+	if np.sqrt(x[j]**2) + (y[j]**2)<=R:
+		n = n + 1
 
-	else y:np.sqrt(X**2) + (Y**2)<=R
-	print y + 1
+	gpi= ((L/R)**2) * (n/N)
+	 
+mifuncion (3, 1, 100)
+ 
 
 	
+
