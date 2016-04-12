13a7cf0528ee97657960fd3b942726e2b7f55968
giselaarrietarivera@gmail.com <giselaarrietarivera@gmail.com>
2016-04-12 11:23:54 -0500

    rwalk2

---
+++ rwalk2.py
@@ -0,0 +1,24 @@
+
+# Gisela Arrieta Rivera
+# 
+
+import numpy as np
+import matplotlib as mplt
+import matplotlib.pyplot as plt
+
+
+# s es un contador
+
+def rwalk1d(N, p):
+	s = 0
+	for i in range(N):
+		l = np.random.random()
+		if l > p: 
+			s = s + 1
+		else:	
+			s = s - 1
+	return s
+
+print 'la posicion final es: ', rwalk1d(1000, 0.2)
+
+
