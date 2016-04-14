e8f2a316d430906196983d21f9f87e385394a288
giselaarrietarivera@gmail.com <giselaarrietarivera@gmail.com>
2016-04-14 11:19:08 -0500

    jfdhdkfj

--- rwalk2.py
+++ rwalk2.py
@@ -1,6 +1,6 @@
 
 # Gisela Arrieta Rivera
-# 
+# 14-04-16
 
 import numpy as np
 import matplotlib as mplt
@@ -8,6 +8,20 @@ import matplotlib.pyplot as plt
 
 
 # s es un contador
+
+n = 100
+N = 1000
+p = 0.5
+h = 2 * N + 1
+
+
+def prom(n):
+	x = N * (2 * P - 1)
+	print 'el promedio es ', x
+
+def des(n):
+
+
 
 def rwalk1d(N, p):
 	s = 0
@@ -19,9 +33,14 @@ def rwalk1d(N, p):
 			s = s - 1
 	return s
 
-print 'la posicion final es: ', rwalk1d(1000, 0.2)
+print 'la posicion final es: ', rwalk1d(1000, 0.5)
 
 
 
 
+
+for i in range(n):
+	x = rwalk1d(N, p)
+	
+	
 
