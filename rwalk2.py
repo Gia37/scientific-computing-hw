d596533fd4e42571a2a7f3a0db22bea43e5c7fa1
giselaarrietarivera@gmail.com <giselaarrietarivera@gmail.com>
2016-04-14 11:38:31 -0500

    hdujkdj

--- rwalk2.py
+++ rwalk2.py
@@ -14,6 +14,24 @@ N = 1000
 p = 0.5
 h = 2 * N + 1
 
+def calcula_media_desviacion(*args):
+    total = 0
+    for i in args:
+        total += i
+    media = total / len(args)
+    total = 0
+    for i in args:
+        total += (i - media) ** 2
+    desviacion = (total / len(args)) ** 0.5
+    return media, desviacion
+
+a, b, c, d = 3, 5, 10, 12
+media, desviacion_tipica = calcula_media_desviacion(a, b, c, d)
+print("Datos:", a, b, c, d)
+print("Media:", media)
+print("Desviación típica:", desviacion_tipica)
+print("Programa terminado")
+
 
 def rwalk1d(N, p):
 	s = 0
